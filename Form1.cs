38228e35c02806e5bdd8b1d15e995d4590829e0c
anabeatrizpereiradearaujo <biabailarina.2006@gmail.com>
2025-04-03 16:17:13 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projeto2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text=="Beatriz" && textBox2.Text == "2424")
+            {
+                MessageBox.Show("Bem vindo ao sistema!");
+            }
+            else
+            {
+                MessageBox.Show("Login ou senha inválidos.");
+            }
+        }
+    }
+}
